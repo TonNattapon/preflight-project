8e109744920ccb1753984a43e906d69e5907d388
Ton Natthapon <nattapon_ton_np@hotmail.com>
2024-07-08 18:01:12 +0700

    Add login page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,18 @@
-import React from 'react';
+import LoginCard from "./components/LoginCard";
 
-function CourseCard({ course }) {
+export default function App() {
   return (
-    <div className="max-w-sm rounded overflow-hidden shadow-lg m-4">
-      <div className="px-6 py-4">
-        <div className="font-bold text-xl mb-2">{course.title}</div>
-        <p className="text-gray-700 text-base">
-          {course.description}
-        </p>
+    <div
+      className="h-screen grid grid-rows-2 bg-cover bg-center"
+      style={{
+        backgroundImage:
+          "url('https://images.unsplash.com/photo-1719917226565-d66c05937ba2?q=80&w=2574&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')",
+      }}
+    >
+      <div className="flex justify-center items-center"></div>
+      <div className="flex justify-center items-center">
+        <LoginCard />
       </div>
     </div>
   );
 }
-
-export default CourseCard;
